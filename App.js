686ff81880ebe8e4d4289be4c078441736c4da2a
Farooque <farooque.reactjs@gmail.com>
2024-01-26 01:52:30 +0530

    third commit JSX

--- App.js
+++ App.js
@@ -1,30 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1", // first argument
-  {
-    id: "title", //came from inside the root div   // second argument
-  }, // this is known as props
-  "This is Parcel" // third argument is children
-); //{} is ? this is write from root child.
+// const heading = React.createElement(
+//   "h1", // first argument
+//   {
+//     id: "title", //came from inside the root div   // second argument
+//   }, // this is known as props
+//   "This is Parcel" // third argument is children
+// ); //{} is ? this is write from root child.
 // <h1 id="title"> Learning React!</h1> This is the same thing but written in JS format
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title", //came from inside the root div
-  },
-  "heading2"
-);
+// const heading2 = React.createElement(
+//   "h2",
+//   {
+//     id: "title", //came from inside the root div
+//   },
+//   "heading2"
+// );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "title", //came from inside the root div
-  },
-  [heading, heading2]
-); //passing children over here
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "title", //came from inside the root div
+//   },
+//   [heading, heading2]
+// ); //passing children over here
+
+//JSX Its a compiler for JavaScript
+
+const heading = (
+  <h1 id="title" key={h1}>This is JSX</h1> 
+  //JSX => React createElement => Object => HTML(DOM)
+)
+
+// React Component =>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -32,7 +41,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 //asyn defer
-root.render(container);
+root.render(heading);
 
 
 
