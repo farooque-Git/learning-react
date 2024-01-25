995af45d412373c37a40003045c3ceae2094f1d5
Farooque <farooque.reactjs@gmail.com>
2024-01-26 01:02:12 +0530

    second day

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
-    "h1",
-    {
-      id: "title", //came from inside the root div
-    },
-    "Bismillah React!"
-  ); //{} is ? this is write from root child.
+  "h1", // first argument
+  {
+    id: "title", //came from inside the root div   // second argument
+  }, // this is known as props
+  "This is Parcel" // third argument is children
+); //{} is ? this is write from root child.
+// <h1 id="title"> Learning React!</h1> This is the same thing but written in JS format
 
-  const heading2 = React.createElement("h2", {
-      {
-      id: "title", //came from inside the root div
-    },
-  }, "heading2");
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title", //came from inside the root div
+  },
+  "heading2"
+);
 
-  const container = React.createElement("div", {
-      {
-      id: "title", //came from inside the root div
-    },
-  }, [heading1, heading2]); //passing children over here
+const container = React.createElement(
+  "div",
+  {
+    id: "title", //came from inside the root div
+  },
+  [heading, heading2]
+); //passing children over here
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  // passing a react element inside the root
-  root.render(container);
+// passing a react element inside the root
+
+
+//asyn defer
+root.render(container);
+
+
+
